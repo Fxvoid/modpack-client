b9833f24e07d105202c590d2f45e4ec0fe0f64cb
Fxvoid <fxvoid803@gmail.com>
2025-12-26 03:39:15 +0200

    fix server error with wool to string recipe

--- kubejs/server_scripts/recipes/wool_to_string.js
+++ kubejs/server_scripts/recipes/wool_to_string.js
@@ -1,22 +1,22 @@
 ServerEvents.recipes(event => {
 
-    // 1. DEFINE THE COMBINED INGREDIENT
-    // This tells KubeJS: "I accept ANY item that is in the Butchery tag OR the FD tag OR the Forge tag."
-    // Add as many tags as you need inside the [ ].
-    const knifeTool = Ingredient.of([
-        '#butchery:knives', 
-        '#farmersdelight:tools/knives',
-        '#forge:tools/knives' 
-    ])
+    // Define all the tags you want to accept
+    const knifeTags = [
+        '#butchery:knives',
+        '#farmersdelight:tools/knives', 
+        '#forge:tools/knives'
+    ]
 
-    // 2. THE RECIPE
-    event.shapeless('4x minecraft:string', [
-        '#minecraft:wool',
-        knifeTool
-    ])
-    // 3. DAMAGE LOGIC
-    // We pass the SAME combined ingredient to .damageIngredient()
-    // This ensures that no matter WHICH specific tag matched, the item will be kept and damaged.
-    .damageIngredient(knifeTool)
-    .id('kubejs:wool_to_string_universal') // Custom ID overrides vanilla checks
+    // Create a unique recipe for EACH tag individually
+    // This avoids the "JsonArray" error completely
+    knifeTags.forEach(tag => {
+        
+        event.shapeless('4x minecraft:string', [
+            '#minecraft:wool', 
+            tag
+        ])
+        .damageIngredient(tag)
+        // Give each one a unique ID based on the tag name so they don't overwrite each other
+        .id(`kubejs:wool_string_from_${tag.replace(/[:/#]/g, '_')}`) 
+    })
 })
