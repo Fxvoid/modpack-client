af06968adfcb11a3beb59d7d763ce23e85964207
Fxvoid <fxvoid803@gmail.com>
2025-12-27 04:36:31 +0200

    remove butchery sulfur

---
+++ kubejs/client_scripts/removed_items/butchery_sulfur.js
@@ -0,0 +1,8 @@
+JEIEvents.hideItems(event => {
+    event.hide([
+        'butchery:sulfur_ore',
+        'butchery:deepslate_sulfur_ore',
+        'butchery:sulfur',
+        'butchery:bottle_of_sulfuric_acid'
+    ])
+})
